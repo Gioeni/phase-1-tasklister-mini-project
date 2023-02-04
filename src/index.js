4536a5824fd8b2a8dc1ada839d49e2a6813879eb
Michael Gioeni <gioenijr@oswego.edu>
2023-02-04 14:20:53 -0700

    initial deliverables text and buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.querySelector("form").addEventListener('submit' , (e) =>{
+e.preventDefault()
+addToDo(e.target.newtaskdescription.value)
 });
+
+function addToDo(task){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  li.textContent = task
+  btn.textContent = 'Delete'
+  li.appendChild(btn)
+  document.querySelector('ul').appendChild(li)
+  //document.querySelector(task).appendChild(btn)
+  //console.log(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+})
